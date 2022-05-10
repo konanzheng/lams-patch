f4fd4a77884620a5112579a76961cae8c13507c8
郑瑞召 <591176192@qq.com>
2022-05-10 16:42:07 +0800

    逐步完善

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## NWG DEMO 把补丁包程序 用Rust + NWG 重新造一边轮子。
+
+
+构建启动不带控制台的程序
+
+``` cargo rustc --release -- -Clink-args="/SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup" ```
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@
     A very simple application that show your name in a message box.
     See `basic` for the version without the derive macro
 */
+#![windows_subsystem = "windows"]
 #[warn(unused_imports)]
 extern crate native_windows_derive as nwd;
 extern crate native_windows_gui as nwg;
@@ -17,7 +18,8 @@ use nwd::NwgUi;
 use nwg::NativeUi;
 // use std::io::{BufReader, BufRead};
 // #[warn(unused_imports)]
-use std::process::{self, Command, Stdio};
+use std::process::{Command, Stdio};
+use std::os::windows::process::CommandExt;
 use std::fs;
 use std::path::*;
 
@@ -34,8 +36,7 @@ pub struct BasicApp {
     // 工程目录
     #[nwg_control(text: "工程目录", size: (60, 40), position: (10, 10), font: Some(&data.font1))]
     prj_label:nwg::Label,
-    // TODO 目前是写死的方便测试，后期要改成 请选择工程目录
-    #[nwg_control(text: "E:\\work\\dagl\\bugfix\\Lams", size: (400, 40), position: (70, 10),readonly: true, font: Some(&data.font1))]
+    #[nwg_control(text: "请选择工程目录", size: (400, 40), position: (70, 10),readonly: true, font: Some(&data.font1))]
     prj_folder: nwg::TextInput,
 
     #[nwg_control(text: "选择", size: (100, 40), position: (490, 10), font: Some(&data.font1))]
@@ -45,8 +46,7 @@ pub struct BasicApp {
 
     #[nwg_control(text: "发布目录", size: (60, 40), position: (10, 60), font: Some(&data.font1))]
     deploy_label:nwg::Label,
-    // TODO 目前是写死的方便测试，后期要改成 请选择发布目录
-    #[nwg_control(text: "C:\\Users\\ruizhao\\Desktop\\0425", size: (400, 40), position: (70, 60),readonly: true, font: Some(&data.font1))]
+    #[nwg_control(text: "请选择发布目录", size: (400, 40), position: (70, 60),readonly: true, font: Some(&data.font1))]
     deploy_folder: nwg::TextInput,
 
     #[nwg_control(text: "选择", size: (100, 40), position: (490, 60), font: Some(&data.font1))]
@@ -106,16 +106,20 @@ impl BasicApp {
         }
     }
     fn refresh(&self) {
-        let output = Command::new("git").current_dir(self.prj_folder.text()).arg("log").arg("--pretty=oneline").arg("-10").output().unwrap();
+        if self.prj_folder.text() == "请选择工程目录" {
+            nwg::simple_message("提示", "请选择工程目录");
+            return;
+        }
+        let output = Command::new("git").creation_flags(0x08000000).current_dir(self.prj_folder.text()).args(["log","--pretty=oneline","-10"]).output().unwrap();
         let out = String::from_utf8(output.stdout).unwrap();
-        println!("{}", out);
+        // println!("{}", out);
         // let mut lines = out.split("/n").collect();
         let mut collect :Vec<String>= Vec::new();
         let mut collect2 :Vec<String>= Vec::new();
         let lines = out.lines();
         for line in lines {
-            let mut format = format!("{},{}", &line[..6], &line[41..]);
-            let mut format2 = format!("{},{}", &line[..6], &line[41..]);
+            let format = format!("{},{}", &line[..6], &line[41..]);
+            let format2 = format!("{},{}", &line[..6], &line[41..]);
             collect.push(format);
             collect2.push(format2);
         }
@@ -123,17 +127,13 @@ impl BasicApp {
         self.new_id.set_selection(Some(0));
         let len = collect2.len()-1;
         self.old_id.set_collection(collect2);
-        self.old_id.set_selection(Some(len));        
-        // String cmd = "git -C \"" + bPath.getAbsolutePath() + "\" log --pretty=oneline -10";
-        // System.out.println(cmd);
-        // List<String> lines = Utils.getExeCmdOutPut(cmd);
-           // 执行git 命令 获取commit 历史
-            //   let mut cmd = std::process::Command::new("git");
+        self.old_id.set_selection(Some(len));
     }
     fn patch(&self) {
         // 1. 判断是否选择了 commit id 和 发布目录
         if self.prj_folder.text() == "请选择工程目录" {
             nwg::simple_message("提示", "请选择工程目录");
+            return ;
         }
         let mut new_id_str = "".to_string();
         match self.new_id.selection_string() {
@@ -159,36 +159,36 @@ impl BasicApp {
             return
         }
         if self.deploy_folder.text() == "请选择发布目录" {
-            nwg::simple_message("提示", "请选择发布目录");
+            nwg::simple_message("发布目录不能为空", "请选择发布目录");
         }
         // 2. 利用git 命令生成 差异信息
-        let output = Command::new("git").arg("diff").arg("--name-status").arg(&old_id_str[..6]).arg(&new_id_str[..6]).current_dir(self.prj_folder.text()).output().unwrap();
+        let output = Command::new("git").creation_flags(0x08000000).args(["diff","--name-status",&old_id_str[..6],&new_id_str[..6]]).current_dir(self.prj_folder.text()).output().unwrap();
         let out = String::from_utf8(output.stdout).unwrap();
-        println!("{}", out);
-        let mut delVec :Vec<&str>= Vec::new();
-        let mut copyVec :Vec<&str>= Vec::new();
+        // println!("{}", out);
+        let mut del_vec :Vec<&str>= Vec::new();
+        let mut copy_vec :Vec<&str>= Vec::new();
         for line in out.lines(){
             let split:Vec<&str> = line.split("\t").collect();
             let flag = split[0].to_string();
             let path = split[1];
-            if !split[1].to_string().starts_with("src"){
+            if !path.to_string().starts_with("src"){
                 continue;
             }
             if flag.starts_with("D") || flag.starts_with("R") {
-                delVec.push(split[1]);
+                del_vec.push(path);
             }
             if flag.starts_with("R") || flag.starts_with("A") ||flag.starts_with("M") {
-                copyVec.push(split[split.len()-1]);
+                copy_vec.push(split[split.len()-1]);
             }
         }
-        let mut deploy_path = self.deploy_folder.text();
-        let mut prj_path = self.prj_folder.text();
+        let deploy_path = self.deploy_folder.text();
+        let prj_path = self.prj_folder.text();
         // TODO 目前实现的很不优雅，后续要处理异常，完善提醒
-        for del in delVec{
+        for del in del_vec{
             del_file(&deploy_path,del);
         }
 
-        for copy in copyVec{
+        for copy in copy_vec{
             copy_file(&deploy_path,&prj_path,copy);
         }
 
@@ -212,19 +212,19 @@ fn transform_path(src:String )->String {
     } else if src.starts_with(JAVA) {
         dest = src.replace(JAVA, WEBINF).replace(".java", ".class");
     }
-    println!("路径转换前:{},转换后:{}",src,dest);
+    // println!("路径转换前:{},转换后:{}",src,dest);
     return dest;
 }
 fn copy_file(to: &str, from:&str,file: &str){
     let mut from2 = from.to_string() +"\\"+ file;
-    let dFile = transform_path(file.to_string());
-    let mut tf = to.to_string()+"\\"+&dFile;
+    let d_file = transform_path(file.to_string());
+    let tf = to.to_string()+"\\"+&d_file;
     if file.starts_with(JAVA) {
-        from2 = (from.to_string() +"\\"+ &dFile).replace(WEBINF, TARGET);
+        from2 = (from.to_string() +"\\"+ &d_file).replace(WEBINF, TARGET);
     }
     // 创建目录
     fs::create_dir_all(Path::new(&tf).parent().unwrap().to_str().unwrap()).unwrap();
-    println!("拷贝从{}到{}",from2,tf);
+    // println!("拷贝从{}到{}",from2,tf);
     fs::copy(&from2, &tf).unwrap();
     // TODO 判断如果是java 文件需要 拷贝内部类
     if file.ends_with(".class") {
@@ -234,16 +234,16 @@ fn copy_file(to: &str, from:&str,file: &str){
         if !parent.is_dir() || !parent.exists() {
             return;
         }
-        let classFiles = parent.read_dir().unwrap();
-        for classFile in classFiles {
-            if let Ok(entry) = classFile {
+        let class_files = parent.read_dir().unwrap();
+        for class_file in class_files {
+            if let Ok(entry) = class_file {
                 let p = entry.path();
                 let name2 = p.file_name().unwrap().to_str().unwrap();
                 let ext = p.extension().unwrap();
                 if ext == "class" && name2.starts_with(name) {
-                    tf = tf.replace(name,name2);
-                    println!("拷贝 从{},到{}",p.to_str().unwrap(),tf);
-                    // fs::copy(cfp.to_str().unwrap(), ).unwrap();
+                    let tf2 = tf.replace(name,name2);
+                    // println!("拷贝 从{},到{}",p.to_str().unwrap(),tf);
+                    fs::copy(cfp.to_str().unwrap(),tf2).unwrap();
                 }
             }
         }
@@ -253,14 +253,14 @@ fn copy_file(to: &str, from:&str,file: &str){
 }
 // 删除文件
 fn del_file(dir: &str,file: &str) {
-    let filePath = dir.to_string()+"\\"+&transform_path(file.to_string());
-    let path = Path::new(&filePath);
+    let file_path = dir.to_string()+"\\"+&transform_path(file.to_string());
+    let path = Path::new(&file_path);
     if path.is_file() {
-        // fs::remove_file(path).unwrap();
-        println!("删除文件:{}",path.to_str().unwrap());
+        fs::remove_file(path).unwrap();
+        // println!("删除文件:{}",path.to_str().unwrap());
     } else if path.is_dir() {
-        // fs::remove_dir_all(path).unwrap();
-        println!("删除目录:{}",path.to_str().unwrap());
+        fs::remove_dir_all(path).unwrap();
+        // println!("删除目录:{}",path.to_str().unwrap());
     }
     // TODO 判断如果是java 文件需要 删除内部类
     if path.extension().unwrap() == "class" {
@@ -269,12 +269,12 @@ fn del_file(dir: &str,file: &str) {
         if !parent.is_dir() || !parent.exists() {
             return;
         }
-        let classFiles = parent.read_dir().unwrap();
-        for classFile in classFiles {
-            let p = classFile.unwrap().path();
+        let class_files = parent.read_dir().unwrap();
+        for class_file in class_files {
+            let p = class_file.unwrap().path();
             if p.extension().unwrap() == "class" && p.file_name().unwrap().to_str().unwrap().starts_with(name) {
-                // fs::remove_file(p).unwrap();
-                println!("删除内部类文件:{}",p.to_str().unwrap());
+                fs::remove_file(p).unwrap();
+                // println!("删除内部类文件:{}",p.to_str().unwrap());
             }
         }
     }
